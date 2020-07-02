01a30855979ba8d967992326e859adb4d36e15e5
Filimon Mikhail <misafilimon255@mail.ru>
2020-07-02 23:50:48 +0300

    fix(js/script.js and realTime/js/script.js)added changes from the previous lesson

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
-    function countTimer(deadline) {
+    const countTimer = deadline => {
         const   timerHours = document.querySelector('#timer-hours'),
             timerMinutes = document.querySelector('#timer-minutes'),
             timerSeconds = document.querySelector('#timer-seconds');
 
-        function getTimeRemaining() {
+        const getTimeRemaining = () => {
             const   dataStop = new Date(deadline).getTime(),
                 dataNow = new Date().getTime(),
                 timeRemaining = (dataStop - dataNow) / 1000,
@@ -15,20 +15,20 @@ window.addEventListener('DOMContentLoaded', () => {
                 minutes = Math.floor((timeRemaining / 60) % 60),
                 hours = Math.floor(timeRemaining / 60 / 60);
             return { seconds, minutes, hours, timeRemaining };
-        }
-        function updateClock() {
-            const timer = getTimeRemaining();
-            function validNull(elem, elemText) {
-                if (elem.toString().length === 1) {
-                    elem = '0' + elem.toString().slice(0);
-                    elemText.textContent = elem;
-                } else {
-                    elemText.textContent = elem;
-                }
+        };
+        const addNull = (elem, elemText) => {
+            if (elem.toString().length === 1) {
+                elem = '0' + elem.toString().slice(0);
+                elemText.textContent = elem;
+            } else {
+                elemText.textContent = elem;
             }
-            validNull(timer.hours, timerHours);
-            validNull(timer.minutes, timerMinutes);
-            validNull(timer.seconds, timerSeconds);
+        };
+        const updateClock = () => {
+            const timer = getTimeRemaining();
+            addNull(timer.hours, timerHours);
+            addNull(timer.minutes, timerMinutes);
+            addNull(timer.seconds, timerSeconds);
 
             if (timer.timeRemaining < 0) {
                 timerHours.textContent = '00';
@@ -37,10 +37,9 @@ window.addEventListener('DOMContentLoaded', () => {
                 // eslint-disable-next-line no-use-before-define
                 clearInterval(interval);
             }
-        }
+        };
         const interval = setInterval(updateClock, 1000);
-    }
-
+    };
     countTimer('04 July 2020');
 
     const toggleMenu =  () => {
