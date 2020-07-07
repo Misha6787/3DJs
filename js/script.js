86fb42129c5e7141027f9f1b14e01c2ea888c61d
Filimon Mikhail <misafilimon255@mail.ru>
2020-07-07 13:56:01 +0300

    feature(js/script.js)complete the lesson24

--- js/script.js
+++ js/script.js
@@ -333,4 +333,51 @@ window.addEventListener('DOMContentLoaded', () => {
         command.addEventListener('mouseout', mouseoutElem);
     };
     hoverImg();
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcCount = document.querySelector('.calc-count'),
+            calcDay = document.querySelector('.calc-day');
+        function scroll(val, el, timeout, step) {
+            let i = 0;
+            (function scrollIns() {
+                if (i <= val) {
+                    setTimeout(scrollIns, timeout);
+                    document.getElementById(el).textContent = i;
+                    i += step;
+                } else {
+                    document.getElementById(el).textContent = val;
+                }
+            })();
+        }
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+            if (calcCount.value > 1) {
+                countValue += (+calcCount.value - 1) / 10;
+            }
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            } else {
+                dayValue *= 1;
+            }
+            if (typeValue && squareValue) {
+                total = Math.round(price * typeValue * squareValue * countValue * dayValue);
+                scroll(total, 'total', 5, 60);
+            }
+        };
+        calcBlock.addEventListener('change', event => {
+            const target = event.target;
+            if (target.matches('select') || target.matches('input')) {
+                countSum();
+            }
+        });
+    };
+    calc();
 });
